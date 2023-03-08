7a313e2eb02f5ede9d96f0fee8d4923b05cf1280
William Alexander <wjalexan@gmail.com>
2023-03-08 12:23:29 +1100

    cleaned unused files

--- serialPrint/serialRead.h
+++ serialPrint/serialRead.h
@@ -1,1 +1,13 @@
-#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+#ifndef SERIALREAD_H
+#define SERIALREAD_H
+
+int serialRead(LPCWSTR port) {
+
+}
+
+
+#endif // SERIALREAD_H
